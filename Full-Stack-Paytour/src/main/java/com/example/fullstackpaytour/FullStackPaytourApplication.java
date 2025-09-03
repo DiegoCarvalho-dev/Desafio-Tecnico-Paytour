432330528cd51b4e16fe81e3dedc867fc60ebb4d
Diego Carvalho <diegoricardo2527@gmail.com>
2025-09-03 15:39:03 -0300

    Projeto criado

---
+++ Full-Stack-Paytour/src/main/java/com/example/fullstackpaytour/FullStackPaytourApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackpaytour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackPaytourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackPaytourApplication.class, args);
+    }
+
+}
---
+++ Full-Stack-Paytour/src/test/java/com/example/fullstackpaytour/FullStackPaytourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackpaytour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackPaytourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
